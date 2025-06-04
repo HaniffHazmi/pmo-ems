59f529d8eb44f2bcd5e5b650c091290e1d227e0b
Haniff Hazmi <haniffhazmi1@gmail.com>
2025-06-04 20:36:32 +0800

    Added sample query for staff and admin

---
+++ query/sample_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO admins (name, email, password) VALUES ('Admin One', 'admin@example.com', 'admin123');
+INSERT INTO staff (name, email, password, matric_no, phone_number) VALUES ('Staff One', 'staff@example.com', 'staff123', 'A123456', '0123456789');
